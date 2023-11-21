7158afce84d83d18e281c3098e87c839e88b033a
Sayd Romey <sayd@null.net>
2023-11-20 21:49:34 -0500

    p_thread array

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:05:12 by cdumais           #+#    #+#             */
-/*   Updated: 2023/11/20 20:48:42 by cdumais          ###   ########.fr       */
+/*   Updated: 2023/11/20 21:45:32 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,25 +33,34 @@ void	*routine(void *arg)
 
 int	main(int argc, char **argv)
 {
-	pthread_t thread_1;
-	pthread_t thread_2;
+	pthread_t	thread[4];
+	int			i;
 
 	(void)argc;
 	(void)argv;
 	// 
 	pthread_mutex_init(&mutex, NULL);
-	// 
-	if (pthread_create(&thread_1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_create(&thread_2, NULL, &routine, NULL) != 0)
-		return (2);
-	if (pthread_join(thread_1, NULL) != 0)
-		return (3);
-	if (pthread_join(thread_2, NULL) != 0)
-		return (4);
-	printf("Value of x: %d\n", x);
-	// 
+	i = 0;
+	while (i < 4)
+	{
+		if (pthread_create(&thread[i], NULL, &routine, NULL) != 0)
+		{
+			perror("pthread_create");
+			return (1);
+		}
+		printf("Thread %d has started\n", i);
+		i++;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		if (pthread_join(thread[i], NULL) != 0)
+			return (2);
+		printf("Thread %d has ended\n", i);
+		i++;
+	}
 	pthread_mutex_destroy(&mutex);
+	printf("Value of x: %d\n", x);
 	// 
 	return (0);
 }
