933d6bec846869337b3af7cb5f17cb5f96c5a5be
Sayd Romey <sayd@null.net>
2023-11-20 20:57:33 -0500

    mutex test

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,28 +6,28 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:05:12 by cdumais           #+#    #+#             */
-/*   Updated: 2023/11/20 16:46:21 by cdumais          ###   ########.fr       */
+/*   Updated: 2023/11/20 20:48:42 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	x = 2;
+int	x = 0;
+pthread_mutex_t mutex;
 
 void	*routine(void *arg)
 {
-	(void)arg;
-	x += 5;
-	sleep(2);
-	printf("Value of x: %d\n", x);
-	return (NULL);
-}
+	int	i;
 
-void	*routine2(void *arg)
-{
 	(void)arg;
-	sleep(2);
-	printf("Value of x: %d\n", x);
+	i = 0;
+	while (i < 10000)
+	{
+		pthread_mutex_lock(&mutex);
+		x++;
+		i++;
+		pthread_mutex_unlock(&mutex);
+	}
 	return (NULL);
 }
 
@@ -39,14 +39,19 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	// 
+	pthread_mutex_init(&mutex, NULL);
+	// 
 	if (pthread_create(&thread_1, NULL, &routine, NULL) != 0)
 		return (1);
-	if (pthread_create(&thread_2, NULL, &routine2, NULL) != 0)
+	if (pthread_create(&thread_2, NULL, &routine, NULL) != 0)
 		return (2);
 	if (pthread_join(thread_1, NULL) != 0)
 		return (3);
 	if (pthread_join(thread_2, NULL) != 0)
 		return (4);
+	printf("Value of x: %d\n", x);
+	// 
+	pthread_mutex_destroy(&mutex);
 	// 
 	return (0);
 }
