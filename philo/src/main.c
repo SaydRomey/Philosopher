2a5200f1e40bba479db28b2db6ed1397876f3ffb
Sayd Romey <sayd@null.net>
2023-11-21 17:22:25 -0500

    multiple mutext test

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,61 +6,60 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:05:12 by cdumais           #+#    #+#             */
-/*   Updated: 2023/11/21 16:18:14 by cdumais          ###   ########.fr       */
+/*   Updated: 2023/11/21 17:04:44 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+#define THREAD_NUM 8
 
-void	*thread_func(void *arg)
+pthread_mutex_t	mutex_fuel;
+int				fuel = 50;
+
+pthread_mutex_t mutex_water;
+int				water = 10;
+
+void	*routine(void *args)
 {
-	int	index;
-	int	sum;
-	int	i;
-
-	index = *(int *)arg;
-	sum = 0;
-	i = 0;
-	while (i < 5)
-	{
-		sum += primes[index + i];
-		i++;
-	}
-	printf("local sum = %d\n", sum);
-	*(int *)arg = sum;
-	return (arg);
+	(void)args;
+	pthread_mutex_lock(&mutex_fuel);
+	pthread_mutex_lock(&mutex_water);
+	fuel += 50;
+	water = fuel;
+	printf("Incremented fuel to %d\n", fuel);
+	printf("Set water to %d\n", water);
+	pthread_mutex_unlock(&mutex_fuel);
+	pthread_mutex_unlock(&mutex_water);
+	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t	thread[2];
+	pthread_t	threads[THREAD_NUM];
 	int			i;
-	int			*a;
-	int			*r;
-	int			sum = 0;
 
 	(void)argc;
 	(void)argv;
+	pthread_mutex_init(&mutex_fuel, NULL);
+	pthread_mutex_init(&mutex_water, NULL);
 	i = 0;
-	while (i < 2)
+	while (i < THREAD_NUM)
 	{
-		a = malloc(sizeof(int));
-		*a = i * 5;
-		if (pthread_create(&thread[i], NULL, &thread_func, a) != 0)
-			return (1);
+		if (pthread_create(&threads[i], NULL, &routine, NULL) != 0)
+			return (printf("Error creating thread\n"), EXIT_FAILURE);
 		i++;
 	}
 	i = 0;
-	while (i < 2)
+	while (i < THREAD_NUM)
 	{
-		if (pthread_join(thread[i], (void **)&r) != 0)
-			return (1);
-		sum += *r;
+		if (pthread_join(threads[i], NULL) != 0)
+			return (printf("Error joining thread\n"), EXIT_FAILURE);
 		i++;
 	}
-	free(r);
-	printf("global sum = %d\n", sum);
-	return (0);
+	printf("Fuel: %d\n", fuel);
+	printf("Water: %d\n", water);
+	pthread_mutex_destroy(&mutex_fuel);
+	pthread_mutex_destroy(&mutex_water);
+	return (EXIT_SUCCESS);
 }
